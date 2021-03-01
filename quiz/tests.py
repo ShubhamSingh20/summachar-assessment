258199bbbb12b5f7a54ed32fb8a3fc7bbb7d148a
Shubham singh <shubham1.singh15@gmail.com>
2021-03-01 20:15:33 +0530

    tests for taking quiz

--- quiz/tests.py
+++ quiz/tests.py
@@ -1,5 +1,5 @@
 from helper.utils import TestEssentials
-from quiz.models import Question, Quiz
+from quiz.models import Question, Quiz, TakenQuiz
 from accounts.models import User
 from django.test import TestCase
 from rest_framework import status
@@ -73,13 +73,63 @@ class QuizTest(TestCase, TestEssentials):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(quiz.questions.count(), 3)
 
-class QuestionTest(TestCase, TestEssentials):
+class QuizTakenTest(TestCase, TestEssentials):
 
     def setUp(self) -> None:
         self.client = APIClient()
 
-    def test_question_create(self) -> None:
-        pass
+    @property
+    def get_quiz_data(self) -> dict:
+        return {
+            'name': 'First Test Quiz',
+            'schedule_date': '2020-02-23 11:15:23',
+            'end_date': '2020-02-28 23:59:59',
+            'description': 'This is the first quiz',
+            'time_per_question': 10,
+            'questions': [
+                {
+                    'question_text': 'First Question',
+                    'question_type': 'mcq',
+                    'answer': 'C',
+                },
+                {
+                    'question_text': 'Second Question',
+                    'question_type': 'open_text',
+                    'answer': 'final',
+                } 
+            ]
+        }
 
-    def test_question_update(self) -> None:
-        pass
+    def test_take_quiz(self) -> None:
+        user = self.make_user_admin(self.jwt_login)
+
+        # create a quiz with questions
+        response = self.client.post(
+            '/api/v1/quizzes/', self.get_quiz_data, 
+            format='json'
+        )
+
+        quiz_slug = response.data.get('id')
+        quiz = Quiz.objects.get(slug=quiz_slug)
+
+        data = {
+            'answers': [
+                {
+                    'question': Question.objects.filter(question_text__startswith='First').first().slug,
+                    'answer': 'b' # incorrect answer !
+                },
+                {
+                    'question': Question.objects.filter(question_text__startswith='Second').first().slug,
+                    'answer': 'final' # correct
+                }
+            ] 
+        }
+
+        response : Response = self.client.post(
+            '/api/v1/quizzes/%s/user_submit/' % quiz_slug, 
+            data, format='json'
+        )
+        taken_quiz = TakenQuiz.objects.get(user=user, quiz=quiz)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(taken_quiz.score, 1) # since one answer is incorrect
+
